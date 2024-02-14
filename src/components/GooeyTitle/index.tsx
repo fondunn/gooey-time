b650f2cb1569b8314472b972e3da9c63778a861d
Denis Karlinsky <keeeeeves@gmail.com>
2024-02-15 00:10:38 +0200

    upt title

--- src/components/GooeyTitle/index.tsx
+++ src/components/GooeyTitle/index.tsx
@@ -2,7 +2,7 @@ import styles from './style.module.scss'
 export const GooeyTitle = () => {
 	return (
 		<div className={styles['wrapper']}>
-			<h1 className={styles.title}>GOOEY Clock</h1>
+			<h1 className={styles.title}>GOOEY TIME</h1>
 		</div>
 	)
 }
